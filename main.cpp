b9a775e1fb6cd693e6a125133253aeaa363a7ac3
Charles Nicholson <charles.nicholson@gmail.com>
2015-05-16 11:00:22 -0700

    more experiments in main

--- main.cpp
+++ main.cpp
@@ -6,15 +6,8 @@
 
 namespace
 {
-    void hello()
-    {
-        std::printf("hello world\n");
-    }
-
-    void add(int a, int b)
-    {
-        std::printf("%d + %d = %d\n", a, b, a + b);
-    }
+    void hello() { std::printf("hello world\n"); }
+    void add(int a, int b) { std::printf("%d + %d = %d\n", a, b, a + b); }
 }
 
 int main(void)
@@ -23,26 +16,42 @@ int main(void)
     dq.dispatch_async(hello);
     dq.dispatch_async(std::bind(add, 123, 456));
 
-    int bound_counter = 0;
-    for (auto i = 0; i < 1024; ++i) {
-        dq.dispatch_async([&]() { ++bound_counter; });
-    }
+    {
+        auto bound_counter = 0;
+        for (auto i = 0; i < 1024; ++i) {
+            dq.dispatch_async([&]() { ++bound_counter; });
+        }
 
-    dq.dispatch_flush();
-    std::printf("bound_counter = %d\n", bound_counter);
+        dq.dispatch_flush();
+        std::printf("bound_counter = %d\n", bound_counter);
+    }
 
     std::printf("dispatch_sync sleep for 1s... ");
     std::fflush(stdout);
     dq.dispatch_sync([] { std::this_thread::sleep_for(std::chrono::milliseconds(1000)); });
     std::printf("and we're back.\n");
 
-    int param_counter = 0;
-    for (auto i = 0; i < 2048; ++i) {
-        dq.dispatch_async(std::bind([](int& c) { ++c; }, std::ref(param_counter)));
+    {
+        auto i = 0;
+        dq.dispatch_async([&] {
+            std::printf("level %d\n", i++);
+            dq.dispatch_async([&] {
+                std::printf("level %d\n", i++);
+                dq.dispatch_async([&] { std::printf("level %d\n", i++); });
+            });
+        });
     }
 
-    dq.dispatch_flush();
-    std::printf("param_counter = %d\n", param_counter);
+    {
+        auto param_counter = 0;
+        for (auto i = 0; i < 2048; ++i) {
+            dq.dispatch_async(std::bind([](int& c) { ++c; }, std::ref(param_counter)));
+        }
+
+        dq.dispatch_flush();
+        std::printf("param_counter = %d\n", param_counter);
+    }
+
     return 0;
 }
 
